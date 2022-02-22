de162c6b64dcc11e11d1517aa7f4361b727d577b
Manasij Haldar <73693494+TheROCKoManz@users.noreply.github.com>
2022-02-22 15:26:25 +0530

    Update Car_Price_Prediction_Linear_Regression.py

--- Car_Price_Prediction_Linear_Regression.py
+++ Car_Price_Prediction_Linear_Regression.py
@@ -7,21 +7,26 @@ from sklearn import linear_model as lm
 df=pd.read_csv(r"C:\Codes\Python\Data\CarPrice_Assignment_modified.csv")        #Path of Car Dataset
 print("PROVIDED DATASET AFTER CLEANING\n\n",df.head(10),"\n\n")
 
-x_train, x_test, y_train, y_test = train_test_split(df[['symboling','fueltype','aspiration','doornumber','carbody','drivewheel',
-                                'enginelocation','wheelbase','enginesize','boreratio', 'stroke',
-                                'compressionratio','horsepower','peakrpm','citympg','highwaympg']],
-                         df[['price']],test_size=0.25)
+x_train, x_test, y_train, y_test = train_test_split(df[['symboling','fueltype','aspiration','doornumber','carbody','drivewheel','enginelocation','wheelbase',
+                                                        'enginesize','boreratio','stroke','compressionratio','horsepower','peakrpm','citympg','highwaympg']],
+                                                    df[['price']],test_size=0.25)
 
 reg=lm.LinearRegression()
 reg.fit(x_train,y_train)
 
 y_predict=reg.predict(x_test)
 
-print("\n\nAccuracy= ",(round(reg.score(x_test,y_test)*100,2)),"%\n\n")
-
-sns.regplot(x = y_test, y = y_predict, scatter_kws = {"color": "blue"},
-                                                            line_kws={"color": "red" }, ci=0)
-
+sns.regplot(x = y_test, y = y_predict, scatter_kws = {"color": "blue"}, line_kws={"color": "red" }, ci=0)
+sns.set_style("white")
+print("\n\nRegression Plot -")
 plt.xlabel('Predicted', color='white')
 plt.ylabel('Truth', color='white')
+plt.show()
+print("\n\nAccuracy= ",(round(reg.score(x_test,y_test)*100,2)),"%\n\n")
+
+correlation=df.corr()
+plt.figure(figsize=(10,10))
+print("\n\nCorrelation -")
+sns.heatmap(correlation,cbar=True,square=True,fmt='.1f',annot=True,annot_kws={'size':8},cmap='BrBG')
+
 plt.show()
